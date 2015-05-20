87485b4008d0ae85ce3f211db818f71bf2df171c
qq729029294 <weijiangnan@126.com>
2015-05-21 03:24:03 +0800

    描述：添加sendMail

---
+++ IntimateAccountServer/src/main/java/com/nan/ia/server/ServerConfigs.java
@@ -0,0 +1,11 @@
+package com.nan.ia.server;
+/**
+ * @ClassName:     ServerConfigs.java
+ * @Description:   配置
+ * 
+ * @author         weijiangnan create on 2015年5月21日 
+ */
+
+public class ServerConfigs {
+
+}
--- IntimateAccountServer/src/main/java/com/nan/ia/server/biz/BizFacade.java
+++ IntimateAccountServer/src/main/java/com/nan/ia/server/biz/BizFacade.java
@@ -10,6 +10,7 @@ package com.nan.ia.server.biz;
 import java.util.Map;
 
 import com.nan.ia.server.entities.RegistionVfCode;
+import com.nan.ia.server.utils.SendMail;
 
 public class BizFacade {
 	Map<String, RegistionVfCode> mapVfCode;
@@ -37,5 +38,27 @@ public class BizFacade {
 //		mapVfCode
 	}
 	
-	//
+	public void sendVerificationCodeByEmail(String mailAddress) {
+        SendMail sendMail = new SendMail();
+        sendMail.setSmtpServer("smtp.126.com");
+        //此处设置登录的用户名
+        sendMail.setUsername("weijiangnan@126.com");
+        //此处设置登录的密码
+        sendMail.setPassword("lj86598095");
+        //设置收件人的地址
+        sendMail.setTo("729029294@qq.com");
+        //设置发送人地址
+        sendMail.setFrom("weijiangnan@126.com");
+        //设置标题
+        sendMail.setSubject("测试邮件标题！");
+        //设置内容
+        sendMail.setContent("你好这是一个带多附件的测试邮件！"); 
+        //粘贴附件
+        //sendMail.attachfile("C:/Login6 (1).jpg");
+        if (sendMail.send())
+        {
+            System.out.println("---邮件发送成功---");
+        }
+	}
+
 }
